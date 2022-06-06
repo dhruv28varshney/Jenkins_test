ea86c66c1bafc5154414e3a7d3e7eba444af0afa
dhruv28varshney <89443690+dhruv28varshney@users.noreply.github.com>
2022-06-06 19:02:20 +0530

    Update run_script.sh

--- char_driver/run_script.sh
+++ char_driver/run_script.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 make host
-jenkins ALL=(ALL) NOPASSWD: insmod ./char_drv_fops.ko
+sudo insmod ./char_drv_fops.ko
 #sudo mknod -m 666 /dev/chardev c 258 0
 echo "testing" > /dev/chardev
 cat /dev/chardev
-jenkins ALL=(ALL) NOPASSWD: rmmod char_drv_fops
+sudo rmmod char_drv_fops
+jenkins ALL=(ALL) NOPASSWD: ALL
+
