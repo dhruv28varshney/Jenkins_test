9db0a501681b2e9cbb17308c0f0a01eab03eb693
dhruv28varshney <89443690+dhruv28varshney@users.noreply.github.com>
2022-06-08 11:14:58 +0530

    Update run_script.sh

--- run_script.sh
+++ run_script.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+make clean
 make host
 sudo insmod ./char_drv_fops.ko
-#sudo mknod -m 666 /dev/chardev c 258 0
-echo "testing" > /dev/chardev
+sudo mknod -m 666 /dev/chardev c 258 0
+echo "testing 1 2 3" > /dev/chardev
 cat /dev/chardev
 sudo rmmod char_drv_fops
